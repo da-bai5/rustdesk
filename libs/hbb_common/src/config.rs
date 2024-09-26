d10423194fcbbf0226f044dd4e09b3ca3c1a2d93
da-bai5 <63036842+da-bai5@users.noreply.github.com>
2024-09-27 02:59:51 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["mtmt.682688.xyz"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["zmzm.682688.xyz"];
 pub const PUBLIC_RS_PUB_KEY: &str = "uAakNogSmn200h0II9AMjNOyhuTjkmo+xarJO++x2Ws=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
